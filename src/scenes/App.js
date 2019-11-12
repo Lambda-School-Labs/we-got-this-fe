e23e103f8547c3d12ed4034b684e69e61657f5e2
sannysc <sannysherief@gmail.com>
2019-11-11 21:29:11 -0800

    Remove inline components.

--- src/scenes/App.js
+++ src/scenes/App.js
@@ -45,54 +45,6 @@ const useStyles = makeStyles(theme => ({
     },
 }));
 
-
-//
-//Config
-var storageRef = Firebase.getStorageRef();
-
-const Dashboard = () => {
-    const [files, setFiles] = useState(null);
-
-    const handleFiles = e => {
-        setFiles(e.target.files);
-    };
-
-    return (
-        <>
-            <h1>Dashboard</h1>{' '}
-            <label htmlFor="fileInput">Select Files to Upload</label>
-            <input
-                type="file"
-                id="fileInput"
-                multiple
-                onChange={e => {
-                    handleFiles(e);
-                }}
-            />
-            {files !== null && (
-                <button
-                    onClick={() => {
-                        console.log('Clicked!');
-                        storageRef
-                            .child('images/me')
-                            .put(files[0])
-                            .then(snapshot => {
-                                console.log(snapshot);
-                            });
-                    }}
-                >
-                    Upload!
-                </button>
-            )}
-        </>
-    );
-};
-
-const Me = () => {
-    const [{ auth }] = useStateValue();
-    return <h1>Me is {auth.currentUser && auth.currentUser.displayName} </h1>;
-};
-
 function App() {
     const [{ auth }, dispatch] = useStateValue();
     const [isLoading, setIsLoading] = useState(true);
