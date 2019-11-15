3d682ceef7def133c28bad169effae29c623f748
Lucas Bazemore <lucasabazemore@gmail.com>
2019-11-14 19:44:31 -0600

    remove JobsList from Dashboard for now

--- src/scenes/Dashboard/index.js
+++ src/scenes/Dashboard/index.js
@@ -39,8 +39,6 @@ const Dashboard = () => {
                     Upload!
                 </button>
             )}
-            {/* <JobsCards /> */}
-            <JobsList />
         </>
     );
 };
