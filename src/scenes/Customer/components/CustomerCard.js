fd33e351e50bd415d55b87cff1ae2a5d4ba20a9a
avi <avkan1087@gmail.com>
2020-03-23 16:48:12 -0400

    ians changes

--- src/scenes/Customer/components/CustomerCard.js
+++ src/scenes/Customer/components/CustomerCard.js
@@ -1,5 +1,5 @@
 import React, {useState, useEffect, useRef} from 'react';
-
+import './style.css';
 //Components
 import EditCustomerForm from '../../../components/forms/EditCustomerForm';
 import DialogWrapper from '../../../components/dialogs/DialogWrapper';
@@ -33,6 +33,11 @@ const Title = styled(Box)({
 	flexDirection: 'row',
 	justifyContent: 'flex-start',
 	alignItems: 'center',
+});
+
+const MediaFlex = styled(Box)({
+	display: 'flex',
+	flexDirection: 'row',
 });
 
 const CustomerDetails = styled(withTheme(Grid))(props => ({
@@ -72,36 +77,61 @@ const CustomerCard = ({customer}) => {
 		`${address.street} ${address.city}, ${address.state} ${address.zipcode}`;
 
 	return (
-		<Grid component={Paper} container item>
-			<Grid item>
-				<CustomerImage
-					img={customer.img}
-					//    https://specials-images.forbesimg.com/imageserve/1026205392/960x0.jpg?)'
-				/>
+		<div className='customerDetails'>
+			<Grid component={Paper} container item>
+				<div>
+					<Grid item className='Image'>
+						<CustomerImage
+							className='ImageSize'
+							img={customer.img}
+							//    https://specials-images.forbesimg.com/imageserve/1026205392/960x0.jpg?)'
+						/>
+					</Grid>
+				</div>
+				<div>
+					<CustomerDetails item>
+						<Title>
+							<Typography variant='h6'>
+								{customer.name}
+							</Typography>
+							<DialogWrapper
+								trigger={click => (
+									<IconButton
+										size='small'
+										onClick={() => click()}
+									>
+										<EditIcon />
+									</IconButton>
+								)}
+								dialogContent={close => (
+									<EditCustomerForm handleClose={close} />
+								)}
+								title='Edit Customer'
+								size='sm'
+							/>
+						</Title>
+						<strong>Address:</strong>{' '}
+						<Typography variant='body2'>{fullAddress}</Typography>
+						<strong>Phone:</strong>{' '}
+						<Typography variant='body2'>
+							{customer.contact.phone}
+						</Typography>
+						<strong>Email:</strong>{' '}
+						<Typography variant='body2'>
+							{customer.contact.email}
+						</Typography>
+						<strong>Payment Type:</strong>{' '}
+						<Typography variant='body2'>
+							{customer.payment}
+						</Typography>
+						<strong>How did they hear about us?</strong>{' '}
+						<Typography variant='body2'>
+							{customer.hearabout}
+						</Typography>
+					</CustomerDetails>
+				</div>
 			</Grid>
-			<CustomerDetails item>
-				<Title>
-					<Typography variant='h6'>{customer.name}</Typography>
-					<DialogWrapper
-						trigger={click => (
-							<IconButton size='small' onClick={() => click()}>
-								<EditIcon />
-							</IconButton>
-						)}
-						dialogContent={close => (
-							<EditCustomerForm handleClose={close} />
-						)}
-						title='Edit Customer'
-						size='sm'
-					/>
-				</Title>
-				<strong>Address:</strong> <Typography variant='body2'>{fullAddress}</Typography>
-				<strong>Phone:</strong> <Typography variant='body2'>{customer.contact.phone}</Typography>
-				<strong>Email:</strong> <Typography variant='body2'>{customer.contact.email}</Typography>
-				<strong>Payment Type:</strong> <Typography variant='body2'>{customer.payment}</Typography>
-				<strong>How did they hear about us?</strong> <Typography variant='body2'>{customer.hearabout}</Typography>
-			</CustomerDetails>
-		</Grid>
+		</div>
 	);
 };
 
