977a84a852fe9c5b2f2188d80464a2ff22869628
Nicole Adams <nicoleadams.boston@gmail.com>
2020-03-24 17:00:59 -0400

    Isolate customer notes and table styling ian merge

--- src/scenes/Customer/components/CustomerNotes.js
+++ src/scenes/Customer/components/CustomerNotes.js
@@ -1,10 +1,10 @@
 import React from 'react';
 
 //Components
-import { Paper, Typography, Grid } from '@material-ui/core';
+import {Paper, Typography, Grid} from '@material-ui/core';
 
 //Styling
-import { makeStyles } from '@material-ui/core/styles';
+import {makeStyles} from '@material-ui/core/styles';
 
 /*
     Displays extra information about a customer and their needs,
@@ -25,24 +25,29 @@ import { makeStyles } from '@material-ui/core/styles';
         }
 */
 const useStyles = makeStyles(theme => ({
-    root: {
-        padding: theme.spacing(1),
-    },
+	root: {
+		padding: theme.spacing(1),
+		[theme.breakpoints.down('xs')]: {
+			display: 'none',
+		},
+	},
 }));
 
-const CustomerNotes = ({ customer }) => {
-    const classes = useStyles();
-    return (
-        <Grid
-            component={Paper}
-            item
-            className={classes.root}
-            justify="space-between"
-        >
-            <Typography variant="h6">Customer Notes</Typography>
-            {customer.notes || 'No notes yet'}
-        </Grid>
-    );
+const CustomerNotes = ({customer}) => {
+	const classes = useStyles();
+	return (
+		<div className='CustomerNotes'>
+			<Grid
+				component={Paper}
+				item
+				className={classes.root}
+				justify='space-between'
+			>
+				<Typography variant='h6'>Customer Notes</Typography>
+				{customer.notes || 'No notes yet'}
+			</Grid>
+		</div>
+	);
 };
 
 export default CustomerNotes;
